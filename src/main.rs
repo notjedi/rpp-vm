6fba455111deaf5b17c7d07b273e1fc00f3be495
Krithic Kumar <krithickumarub@protonmail.com>
2022-11-02 23:52:59 +0530

    refactor: restructure `TokenKind` to a submodule in lexer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod lexer;
 
+use lexer::tokens::TokenKind;
+
 fn main() {
-    println!("{}", lexer::TokenKind::ProgramStart);
-    println!("{}", lexer::TokenKind::Number(42));
-    println!("{}", lexer::TokenKind::Ident("foo".to_string()));
+    println!("{}", TokenKind::ProgramStart);
+    println!("{}", TokenKind::Number(42));
+    println!("{}", TokenKind::Ident("foo".to_string()));
     println!(
         "{}",
-        lexer::TokenKind::Literal("Meaning of life is 42?".to_string())
+        TokenKind::Literal("Meaning of life is 42?".to_string())
     );
-    println!("{:?}", lexer::TokenKind::ProgramStart);
+    println!("{:?}", TokenKind::ProgramStart);
 }
